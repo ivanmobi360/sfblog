91d1d236883d6cc4c1930be41c87c163c88dfbf1
irodriguez <irodriguez@mobi360.ca>
2013-04-22 15:37:42 -0500

    functional tests

--- src/Blogger/BlogBundle/Tests/Controller/PageControllerTest.php
+++ src/Blogger/BlogBundle/Tests/Controller/PageControllerTest.php
@@ -15,5 +15,22 @@ class PageControllerTest extends WebTestCase {
 		$this->assertEquals(1, $crawler->filter('h1:contains("About sfblog")')->count());
 	}
 	
+	function testIndex()
+	{
+		$client = static::createClient();
+		$crawler = $client->request('GET', '/');
+		$this->assertTrue($crawler->filter('article.blog')->count()>0);
+		
+		//find first link
+		$blogLink = $crawler->filter('article.blog h2 a')->eq(1);//first();
+		$blogTitle = $blogLink->text();
+		//echo $blogLink->link()->getUri();
+		$crawler = $client->click($blogLink->link());
+		//$crawler = $client->request( 'GET', $blogLink->link()->getUri());
+		
+		//$this->assertEquals(1, $crawler->filter('h2:contains("' . $blogTitle . '")' )->count());
+		
+	}
+	
 }
 
